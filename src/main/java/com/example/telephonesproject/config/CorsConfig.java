778a2611299a2ff422ee216bc7ab838617a960ac
Ahmed Kadar <ahmkadar@emeal.nttdata.com>
2025-05-20 20:06:15 +0100

    verify corsConfig again

--- src/main/java/com/example/telephonesproject/config/CorsConfig.java
+++ src/main/java/com/example/telephonesproject/config/CorsConfig.java
@@ -1,19 +1,22 @@
 package com.example.telephonesproject.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// Configuration Spring Boot
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "https://stalwart-nougat-e2115d.netlify.app"                  // Pour le dev local
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
     }
 }
